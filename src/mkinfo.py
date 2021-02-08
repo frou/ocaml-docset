b9bef3fe9c6209a552c8004c2b7b6a76d7dcba4a
Duncan Holm <duncan@holm.scot>
2021-02-08 12:32:08 +0000

    Have "Open Online Page" go to versioned pages on ocaml.org
    Fixes #1

--- src/mkinfo.py
+++ src/mkinfo.py
@@ -3,7 +3,7 @@
 import plistlib
 import sys
 
-_, name, readable_name, search_keyword, main_page = sys.argv
+_, name, readable_name, search_keyword, main_page, release_url = sys.argv
 
 # REF: https://kapeli.com/docsets (search for "Info.plist")
 
@@ -15,11 +15,7 @@ d = {
     "isDashDocset": True,
     "dashIndexFilePath": main_page,
     "DashDocSetFamily": "dashtoc",
+    "DashDocSetFallbackURL": release_url,
 }
 
 plistlib.dump(d, sys.stdout.buffer)
-
-# @todo Get the "Open Online Page" functionality hooked up to use ocaml.org
-# @body https://kapeli.com/docsets#onlineRedirection
-# @body https://ocaml.org/releases/4.11/htmlman/index.html
-# @body That functionality is actually already active, despite the key not being in the plist. But it goes through a Kapeli redirect and ends up at caml.inria.fr. I think this is because Dash natively has some knowledge of OCaml (due to the official outdated docset) that it's falling back on.
