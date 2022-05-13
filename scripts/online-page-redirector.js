94c82b81971dc1af37de3996907ebde41c8955a8
Duncan Holm <duncan@holm.scot>
2022-05-13 14:09:19 +0100

    First cut at the redirection logic

--- scripts/online-page-redirector.js
+++ scripts/online-page-redirector.js
@@ -2,16 +2,35 @@
 // needed to make the "Open Online Page" and "Copy Online Page URL" Dash
 // features work properly for this docset.
 //
-// The reason we need to do this is because the V3 redesign of the ocaml.org
-// website changed things such that the URLs of pages in the online version of
-// the manual no longer use the same directory structure as the .tar.gz version
-// of the manual, which this docset is built from.
+// REF: https://deno.com/deploy/docs
+
+// The reason we need to do this is because the 2022 redesign ("V3") of the
+// ocaml.org website changed things such that the URLs of pages in the online
+// version of the manual no longer use the same directory structure as
+// the .tar.gz version of the manual, which this docset is built from.
 
 import { serve } from "https://deno.land/std@0.139.0/http/server.ts"
 
-console.log("Listening on http://localhost:8000")
-serve(_req => {
-  return new Response("Hello Dash World!", {
-    headers: { "content-type": "text/plain" },
-  })
+const reqPathLibraryPagePattern = new URLPattern({
+  pathname: "/:version/htmlman/libref/:page",
+})
+const reqPathOtherPagePattern = new URLPattern({
+  pathname: "/:version/htmlman/:page",
+})
+
+const respUrlPrefix = "https://ocaml.org/releases"
+
+serve(req => {
+  let match = reqPathLibraryPagePattern.exec(req.url)
+  if (match) {
+    return Response.redirect(
+      `${respUrlPrefix}/${match.groups.version}/api/${match.groups.page}`
+    )
+  } else if ((match = reqPathOtherPagePattern.exec(req.url))) {
+    return Response.redirect(
+      `${respUrlPrefix}/${match.groups.version}/${match.groups.page}`
+    )
+  } else {
+    return Response.error()
+  }
 })
