db1d89b0e2d8a6ba605bed1c285442e461d2c41c
Duncan Holm <duncan@holm.scot>
2022-05-17 14:09:53 +0100

    Give a fuller explanation at the top; Tweak serving code and error msg

--- scripts/online-page-redirector.js
+++ scripts/online-page-redirector.js
@@ -1,52 +1,72 @@
-// This is a "Serverless" / Cloud function that contains some supporting logic
-// needed to make the "Open Online Page" and "Copy Online Page URL" Dash
-// features work properly for this docset. Deno's platform notices when a new
-// commit is made to the GitHub repo and automatically redeploys this file.
-// REF: https://deno.com/deploy/docs/deployments
+/*
+This is a "Serverless"/Cloud function that contains some supporting logic needed to make
+the "Open Online Page" and "Copy Online Page URL" Dash features work properly for this
+Docset. The Deno Deploy platform notices when a new commit is made to the GitHub repo
+and automatically redeploys this file. REF: https://deno.com/deploy/docs/deployments
 
-// The reason we need to do any of this is that the 2022 redesign ("V3") of the
-// ocaml.org website changed things such that the URLs of pages in the online
-// version of the manual no longer use the same directory structure as
-// the .tar.gz version of the manual (which our docset is built from).
+The reason we need to do any of this is that the 2022 redesign ("V3") of the ocaml.org
+website changed things such that the URLs of pages in the online version of the manual
+no longer use the same directory structure as the .tar.gz version of the manual
+(which our Docset is generated from).
+
+For example:
+
+When the Docset is generated (using the Makefie), it is for a specific OCaml version,
+and the `DashDocSetFallbackURL` value in its Info.plist file is set to something like:
+
+    https://ocaml-docset-redirector.deno.dev/4.14/
+
+When a user of the Docset is viewing a page inside the Docset using Dash (such as
+`htmlman/libref/Arg.html`), and they use the "Open Online Page" feature (by clicking the
+Share icon at the top right), this URL will be opened in their default web browser:
+
+    https://ocaml-docset-redirector.deno.dev/4.14/htmlman/libref/Arg.html
+
+The logic in this file will receive that HTTP request and will transform that URL into
+the following URL, and redirect to it:
+
+    https://ocaml.org/releases/4.14/api/Arg.html
+*/
 
 import { serve } from "https://deno.land/std@0.139.0/http/server.ts"
-import {
-  Status,
-  STATUS_TEXT,
-} from "https://deno.land/std@0.139.0/http/http_status.ts"
+import { Status } from "https://deno.land/std@0.139.0/http/http_status.ts"
 import * as strTags from "https://cdn.skypack.dev/common-tags@1.8.2?dts"
 
-const reqPathLibraryPagePattern = new URLPattern({
-  pathname: "/:version/htmlman/libref/:page",
-})
-const reqPathOtherPagePattern = new URLPattern({
-  pathname: "/:version/htmlman/:page",
-})
+const ocamlReleasesUrl = "https://ocaml.org/releases"
 
-const respUrlPrefix = "https://ocaml.org/releases"
+const selfExplainerUrl =
+  "https://github.com/frou/ocaml-docset/blob/master/scripts/online-page-redirector.js"
 
-serve(req => {
-  let match = reqPathLibraryPagePattern.exec(req.url)
-  if (match) {
-    return Response.redirect(
-      `${respUrlPrefix}/${match.pathname.groups.version}/api/${match.pathname.groups.page}`
-    )
-  } else if ((match = reqPathOtherPagePattern.exec(req.url))) {
-    return Response.redirect(
-      `${respUrlPrefix}/${match.pathname.groups.version}/manual/${match.pathname.groups.page}`
-    )
-  } else {
-    const statusCode = Status.NotFound
-    return new Response(
-      strTags.oneLine`If you arrived here via the 'Open Online Page' or
-'Copy Online Page URL' feature in Dash, then this should have worked.
-Please open a new issue at https://github.com/frou/ocaml-docset/issues
-saying which page of the docset you were trying to view.`,
-      {
-        headers: { "content-type": "text/plain" },
-        status: statusCode,
-        statusText: STATUS_TEXT.get(statusCode),
-      }
-    )
+const requestRoutes = [
+  [
+    new URLPattern({ pathname: "/:version/htmlman/libref/:page" }),
+    match =>
+      Response.redirect(
+        `${ocamlReleasesUrl}/${match.pathname.groups.version}/api/${match.pathname.groups.page}`
+      ),
+  ],
+  [
+    new URLPattern({ pathname: "/:version/htmlman/:page" }),
+    match =>
+      Response.redirect(
+        `${ocamlReleasesUrl}/${match.pathname.groups.version}/manual/${match.pathname.groups.page}`
+      ),
+  ],
+]
+
+serve(function (req, connInfo) {
+  for (const [pattern, handler] of requestRoutes) {
+    const match = pattern.exec(req.url)
+    if (match) {
+      return handler(match)
+    }
   }
+
+  console.warn({ unhandledUrlRequested: req.url, by: connInfo.remoteAddr.hostname })
+  return new Response(
+    strTags.oneLine`Unrecognised path. <a href="${selfExplainerUrl}">See here</a> for an
+    explanation of the purpose of this service, and open an issue if it is not working
+    properly for you.`,
+    { status: Status.NotFound, headers: { "Content-Type": "text/html" } }
+  )
 })
